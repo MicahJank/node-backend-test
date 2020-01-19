e7334dd6dc0e5e3f93378b78fc3406943a577947
MicahJank <MicahJank@users.noreply.github.com>
2020-01-19 14:11:45 -0600

    added cors and helmet middleware

--- api/server.js
+++ api/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
-
 const server = express();
+
+const cors = require('cors');
+const helmet = require('helmet');
+
+
+server.use(cors());
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send({
+        api: 'Its worrrking, its wooooorking!'
+    })
+})
 
 module.exports = server;
