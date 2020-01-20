442fedd4fb05a57c9cec050b7ff70b4e2af7e185
MicahJank <MicahJank@users.noreply.github.com>
2020-01-20 16:59:02 -0600

    added firebase config to a firebase file and used that object in initializeApp for firebase

---
+++ client/src/config/firebase.js
@@ -0,0 +1,11 @@
+
+// used in index.js
+export const fireBaseConfig = {
+    apiKey: "AIzaSyAkAz4EMio1xFP1-6ABn80PFm1U1udZt1E",
+    authDomain: "node-auth-testing.firebaseapp.com",
+    databaseURL: "https://node-auth-testing.firebaseio.com",
+    projectId: "node-auth-testing",
+    storageBucket: "node-auth-testing.appspot.com",
+    messagingSenderId: "1074400465053",
+    appId: "1:1074400465053:web:f3109e62f1703b1ddd8474"
+  };
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import firebase here
+import firebase from 'firebase';
+
+// firebase config
+import { fireBaseConfig } from './config/firebase.js';
+
+firebase.initializeApp(fireBaseConfig);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
