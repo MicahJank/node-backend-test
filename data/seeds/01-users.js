1dcc9606846abbd263eca0099ea0006aac97462d
MicahJank <MicahJank@users.noreply.github.com>
2020-01-20 12:22:06 -0600

    filled out seed files to be able to run

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,13 +1,26 @@
-
+// These default users are NOT secure - the passwords being saved into the database are merely strings and have not been hashed. Therefore it will not work
+// to log in with these users.
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('users').insert([
+    {
+      username: 'fakeUser1',
+      email: 'fakeEmail1',
+      password: 'hashPassWord'
+    },
+    {
+      username: 'fakeUser2',
+      email: 'fakeEmail2',
+      password: 'hashPassWord'
+    },
+    {
+      username: 'fakeUser3',
+      email: 'fakeEmail3',
+      password: 'hashPassWord'
+    },
+    {
+      username: 'fakeUser4',
+      password: 'hashPassWord'
+    }
+  ]);
+  
 };
